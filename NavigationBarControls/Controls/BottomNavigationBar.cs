d75cc562b86d15db912c86215b502028cf2a6db7
nevelate <isfandiyorakbarov450@gmail.com>
2024-07-03 15:11:12 +0500

    Added transitions

--- NavigationBarControls/Controls/BottomNavigationBar.cs
+++ NavigationBarControls/Controls/BottomNavigationBar.cs
@@ -10,6 +10,16 @@ namespace NavigationBarControls.Controls
 {
     public class BottomNavigationBar : ItemsControl
     {
+        public static readonly StyledProperty<double> SpacingProperty =
+           AvaloniaProperty.Register<StackPanel, double>(nameof(Spacing));
 
+        /// <summary>
+        /// Gets or sets the size of the spacing to place between child controls.
+        /// </summary>
+        public double Spacing
+        {
+            get => GetValue(SpacingProperty);
+            set => SetValue(SpacingProperty, value);
+        }
     }
 }
